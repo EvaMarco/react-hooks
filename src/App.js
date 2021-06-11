969940882f8407eb14a8fadc5b5d8225a3554017
Eva Marco <eva.marco@a3sec.com>
2021-06-11 21:00:29 +0200

    move use state of dark mode to father

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Header from './components/Header';
-import Character from './components/Characters';
+import Characters from './components/Characters';
+import React, { useState } from 'react';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const handleClick = () =>{
+      setDarkMode(!darkMode);
+  }
   return (
     <div className="App">
-      <Header />
-      <Character />
+      <Header handleClick={handleClick} darkMode={darkMode}/>
+      <Characters darkMode={darkMode} />
     </div>
   );
 }
