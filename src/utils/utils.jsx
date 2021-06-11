984e2afa6c60052d62f86e8190e685ba55203261
Eva Marco <eva.marco@a3sec.com>
2021-06-11 21:12:09 +0200

    Cretae icon mapping for character

---
+++ src/utils/utils.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSkullCrossbones, faHeartbeat, faQuestion } from '@fortawesome/free-solid-svg-icons';
+import { faVenus, faMars, faGenderless, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+
+
+const statusMapping = {
+    'Alive': <FontAwesomeIcon icon={faHeartbeat}/>,
+    'Dead': <FontAwesomeIcon icon={faSkullCrossbones}/>,
+    'unknown': <FontAwesomeIcon icon={faQuestion}/>
+};
+
+const genderMapping = {
+    'Female': <FontAwesomeIcon icon={faVenus}/>,
+    'Male': <FontAwesomeIcon icon={faMars}/>,
+    'Genderless': <FontAwesomeIcon icon={faGenderless}/>,
+    'unknown': <FontAwesomeIcon icon={faQuestionCircle}/>,
+}
+
+export { statusMapping, genderMapping};
