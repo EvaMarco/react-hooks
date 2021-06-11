cf96d860238f1c9823c51a9c4597c4669fa33018
Eva Marco <eva.marco@a3sec.com>
2021-06-11 20:59:54 +0200

    Add fetch to characters

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,15 +1,27 @@
 import React, { useState, useEffect } from 'react'
 
-export const Characters = () => {
+import CharTag from './CharTag';
+
+import './characters.css';
+
+export const Characters = ({darkMode}) => {
 
     const [characters, setCharacters] = useState([]);
     const [error, setError] = useState(false);
 
-    useEffect(()=>{
-        fetch("https://rickandmortyapi.com/api/character/")
+    const fetchCharacters = (url) =>{
+        fetch(url)
             .then(response => response.json())
-            .then(data => {setCharacters(data.results); setError(false);})
+            .then(data => {
+                setCharacters(data.results); setError(false);
+                if(data.info.next) setNextUrl(data.info.next);
+                if(data.info.prev) setPreviewUrl(data.info.prev);
+            })
             .catch(error =>  setError(true));
+    }
+
+    useEffect(()=>{
+        fetchCharacters("https://rickandmortyapi.com/api/character/");
     }, []);
     if(error){
         return(
@@ -17,11 +29,11 @@ export const Characters = () => {
         )
     }
     return (
-        <div className="Characters">
-            {characters.map(character =>
-                <h2 key={character.id}>{character.name}</h2>
-            )}
-        </div>
+            <div className={`characters ${darkMode? "darkMode" : ""}`}>
+                {characters.map(character =>
+                    <CharTag key={character.id} character={character} darkMode={darkMode}/>
+                )}
+            </div>
     )
 }
 
