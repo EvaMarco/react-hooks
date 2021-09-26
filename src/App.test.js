dba35d7d1c216ce11ebd218870be607ff1f07df7
eva_marco <eva.marco@a3sec.com>
2021-09-26 17:28:22 +0200

    create basic test for header

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen, fireEvent } from "@testing-library/react";
+import ThemeContext from "./context/ThemeContext";
+import Header from "./components/header/Header";
 
 test("renders Header", () => {
-    render(<App />);
+    const mockedUserDispatch = jest.fn();
+    render(
+        <ThemeContext.Provider value={{ darkMode: false, setDarkMode: mockedUserDispatch }}>
+            <Header />
+        </ThemeContext.Provider>
+    );
     const headerText = screen.getByText(/Curso Profesional de React Hooks/i);
     expect(headerText).toBeInTheDocument();
+});
+
+test("Find button and change to dark Mode", () => {
+    const mockedUserDispatch = jest.fn();
+    render(
+        <ThemeContext.Provider value={{ darkMode: false, setDarkMode: mockedUserDispatch }}>
+            <Header />
+        </ThemeContext.Provider>
+    );
+    const headerBtn = screen.getByRole("switch");
+    expect(headerBtn).not.toBeDisabled();
+    fireEvent.click(headerBtn);
+    expect(mockedUserDispatch).toHaveBeenCalled();
 });
